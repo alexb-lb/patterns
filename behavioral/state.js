d5d22f2e860dd05ff3125df0dea1178b1f9c4a27
Alex <oknerbob@gmail.com>
2018-09-12 00:58:48 +0300

    state

---
+++ behavioral/state.js
@@ -0,0 +1,7 @@
+/**
+ * State pattern
+ *
+ * It is a behavioural design pattern that allows an object to alter its behaviour based on changes to its internal state.
+ * The object returned by a State pattern class seems to change its class. It provides state-specific logic to a
+ * limited set of objects in which each object type represents a particular state.
+ */
